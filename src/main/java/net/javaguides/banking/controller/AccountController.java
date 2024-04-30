acb314948d784105d646a22c823bbdc237b6812d
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-04-30 17:09:25 +0200

    Deposit REST API

--- src/main/java/net/javaguides/banking/controller/AccountController.java
+++ src/main/java/net/javaguides/banking/controller/AccountController.java
@@ -1,10 +1,12 @@
 package net.javaguides.banking.controller;
 
+import java.util.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,14 @@ public class AccountController {
     @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable("id") Long id){
         AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    //Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id,@RequestBody Map<String,Double> request){
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(id,amount);
         return ResponseEntity.ok(accountDto);
     }
     
