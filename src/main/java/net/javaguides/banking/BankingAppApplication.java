edf8b2300b4cdf92763addd0a78bd5b664c2258a
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-04-30 16:45:17 +0200

    Get Account REST API

--- src/main/java/net/javaguides/banking/BankingAppApplication.java
+++ src/main/java/net/javaguides/banking/BankingAppApplication.java
@@ -8,7 +8,7 @@ public class BankingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
-		System.out.println("Project listening port 8080...");
+		System.out.println("Project listening port 8081...");
 	}
 
 }
